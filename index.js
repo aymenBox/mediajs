61a009ec51b1108eb8c507961c72c0ddda72b632
aymenBox <boxaymen7@gmail.com>
2020-11-12 12:12:22 +0100

    adding record and download function

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
     var sahrebtn = document.getElementById("share");
     var stopShare = document.getElementById("stopShare");
+    var recordbtn = document.getElementById("recordbtn");
+    var stoprecordbtn = document.getElementById("stoprecordbtn");
     var videoElement = document.getElementById("videoElement");
+    var recordedvideo = document.getElementById("recordedvideo");
+    var downloadurl = document.getElementById("downloadurl");
+
     sahrebtn.addEventListener("click",(evt)=>{
         mediajs(videoElement).startCapture(null);
     });
     stopShare.addEventListener("click",(evt)=>{
         mediajs(videoElement).stopCapture();
     });
+    recordbtn.addEventListener("click",(evt)=>{
+        mediajs(videoElement).startRecording(videoElement,stoprecordbtn).then(recordedChunks=>{
+            mediajs(videoElement).downloadstrem(recordedChunks,downloadurl,recordedvideo)
+        });
+    })
--- media.js
+++ media.js
@@ -17,12 +17,43 @@ function mediajs(stremObj){
              return null;
          }
      }
+
      self.stopCapture=function(evt){
         let trak = stremObj.srcObject.getTracks();
         trak.forEach(trak => {
             trak.stop()
         });
         stremObj.srcObject=null;
+    }
+
+    self.startRecording = function(stremObj,stopRecord){
+        stremObj.srcObject.captureStream = stremObj.srcObject.captureStream || stremObj.srcObject.mozCaptureStream;
+        let recorder = new MediaRecorder(stremObj.srcObject);
+        let data=[];
+        /**creat an event lisgner ondataavailable that 
+         * eaisly push data to the data array wich is the blob (binary lare object)
+         */
+        recorder.ondataavailable = event => data.push(event.data);
+        recorder.start();
+        let stopped = new Promise((resolve, reject) => {
+          recorder.onstop = resolve;
+          recorder.onerror = event => reject(event.name);
+        });
+        stopRecord.addEventListener("click",(evt)=>{
+            recorder.stop()
+           // return data;
+        });
+        return Promise.all([
+          stopped,
+        ])
+        .then(() => data);
+    }
+
+    self.downloadstrem = function (recordedChunks,download,recordedVideo){
+        let recordedBlob = new Blob(recordedChunks, { type: "video/mp4" });
+        recordedVideo.src = URL.createObjectURL(recordedBlob);
+        download.href = recordedVideo.src;
+        download.download = "RecordedVideo.mp4"; 
     }
     return self;
 }
