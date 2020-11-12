9defad4af73a2b5a61f2cc5d23f8c0c21c587a76
aymenBox <boxaymen7@gmail.com>
2020-11-12 14:30:15 +0100

    adding comment to the index.js file

--- index.js
+++ index.js
@@ -6,12 +6,18 @@
     var downloadurl = document.getElementById("downloadurl");
 
     sahrebtn.addEventListener("click",(evt)=>{
+        //this function will ask the user permution to capteur the screen 
         mediajs(videoElement).startCapture(null);
     });
     stopShare.addEventListener("click",(evt)=>{
+        //this function will stop screen capteur
         mediajs(videoElement).stopCapture();
     });
     recordbtn.addEventListener("click",(evt)=>{
+        //the startRocording function will start recording the data outputed to the videoElement
+        //then resolve the result to the downloadstrem function that takes tow peremeter 
+        //the recordedChunks resulted from the recording function and a url html element to 
+        //download the stream later
         mediajs(videoElement).startRecording(stoprecordbtn).then(recordedChunks=>{
             mediajs(videoElement).downloadstrem(recordedChunks,downloadurl)
         });
